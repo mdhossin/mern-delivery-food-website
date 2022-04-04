11cb58091cadd49cab8dda6f59eb51cf863817d8
md sahadat hossin <mdsahadathossin778@gmail.com>
2022-04-04 21:59:42 +0600

    simple delicious section is done

--- client/src/components/SimpleDelicious/SimpleDelicious.jsx
+++ client/src/components/SimpleDelicious/SimpleDelicious.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const SimpleDelicious = () => {
-  return <div>simple</div>;
+  return (
+    <div className="section">
+      <div className="simple">
+        <div className="simple__overlay"></div>
+
+        <div className="simple__content container-div">
+          <p>Enjoy Great Recipe</p>
+          <h3>Simple And Delicious Food</h3>
+          <h5>Flat 20% Discount</h5>
+          <button className="button">Shop Now</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SimpleDelicious;
